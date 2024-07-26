e51c526382b03ed085466b91a2ec9cae1a8c0123
dflorian251 <inf2021044@ionio.gr>
2024-07-26 14:01:26 +0300

    Filtering wishes.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,19 +17,31 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'wish-list';
-  newWishText ='';
-
   wishes:  WishItem[] = [
     new WishItem('Na parw kafe'),
     new WishItem('Na parw cafeihnh', true),
     new WishItem('Na parw mia Vespa Primavera', false),
   ];
 
+  listFilter : String = '0';
+  newWishText ='';
+  title = 'wish-list';
+  visibleWishes : WishItem[] = this.wishes;
+
   addNewWish() {
     // add the new created wish
     this.wishes.push(new WishItem(this.newWishText));
     this.newWishText = '';
+  }
+
+  filterChanged(value : any) {
+    if (value === '0') {
+       this.visibleWishes = this.wishes;
+    } else if (value === '1') {
+      this.visibleWishes = this.wishes.filter(wish => !wish.isComplete);
+    } else {
+      this.visibleWishes = this.wishes.filter(wish => wish.isComplete);
+    }
   }
 
   toogleWish(wish : WishItem) {
