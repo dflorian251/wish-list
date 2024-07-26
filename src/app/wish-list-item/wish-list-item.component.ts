c157670bff7de3d86d8bc91143f216df350b95d3
dflorian251 <inf2021044@ionio.gr>
2024-07-26 16:24:02 +0300

    Styling on wish list items.

--- src/app/wish-list-item/wish-list-item.component.ts
+++ src/app/wish-list-item/wish-list-item.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { WishItem } from '../../shared/models/wishItem';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'wish-list-item',
   standalone: true,
-  imports: [],
+  imports: [
+    NgClass,
+  ],
   templateUrl: './wish-list-item.component.html',
   styleUrl: './wish-list-item.component.css'
 })
@@ -13,6 +16,10 @@ export class WishListItemComponent {
   @Input() fullfilled! : boolean;
 
   @Output() fullfilledChange = new EventEmitter<boolean>();
+
+  getCssClasses() {
+    return this.fullfilled ? ['strikeout', 'text-muted'] : [];
+  }
 
   toogleWish() {
     this.fullfilled = !this.fullfilled;
