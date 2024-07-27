4b853ea0711fe3dce33590c35104556787528dca
dflorian251 <inf2021044@ionio.gr>
2024-07-27 23:52:46 +0300

    Dependencies tranfered to wish module.

--- src/app/wish/wish.module.ts
+++ src/app/wish/wish.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { WishlistComponent } from './wishlist/wishlist.component';
+import { AddWishFormComponent } from './add-wish-form/add-wish-form.component';
+import { WishListItemComponent } from './wish-list-item/wish-list-item.component';
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    WishListItemComponent,
+    AddWishFormComponent,
+    WishlistComponent,
   ]
 })
 export class WishModule { }
