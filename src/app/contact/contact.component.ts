5e3870d5c6f7b0dee5968d1cadae991edc168650
dflorian251 <inf2021044@ionio.gr>
2024-07-28 11:55:12 +0300

    Form is now reactive

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+  ],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
 
+  contactForm = new FormGroup({
+    senderName: new FormControl(''),
+    senderEmail: new FormControl(''),
+    senderMessage: new FormControl(''),
+  })
+
+  submitForm() {
+    console.log(this.contactForm.value);
+    // if (this.senderNameControl.dirty) {
+    //   alert('You chaned the name field.');
+    // }
+  }
 }
